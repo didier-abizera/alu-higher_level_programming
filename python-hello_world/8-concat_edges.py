f1cb60fe76bd8b0104abf3b3c66ccdd18d6e054f
didier-abizera <d.abizera@alustudent.com>
2026-02-26 15:18:01 -0800

    fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 word = "object-oriented programming with Python"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-print(word_first_3 + word[3:-2] + word_last_2)
+print(word[0:6] + word[7:])
