a114805f4e510702ad30a8493e044becf6853012
didier-abizera <d.abizera@alustudent.com>
2026-02-26 19:58:01 -0800

    Fix 3-print_alphabt.py use format

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join([chr(i) for i in range(97, 123) if chr(i) != 'e' and chr(i) != 'q']), end="")
+print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) != 'e' and chr(i) != 'q'), end="")
