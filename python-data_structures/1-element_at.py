256c590ac2bc1a57240078e46dc15c3cb231363a
didier-abizera <d.abizera@alustudent.com>
2026-03-11 08:18:43 -0700

    solving the eeror

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Prints all integers of a list"""
-    for i in my_list:
-        print("{:d}".format(i))
+def element_at(my_list, idx):
+    """Retrieves an element from a list"""
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
