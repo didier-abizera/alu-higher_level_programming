97f4fe9f5e3ac1cdc8eac8f124f75d705d771dec
didier-abizera <d.abizera@alustudent.com>
2026-03-12 22:38:28 -0700

    Fix uniq_add function

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
+"""Module for uniq_add function"""
+
+
 def uniq_add(my_list=[]):
     """Adds all unique integers in a list"""
-    return sum(set(my_list))
-```
-
----
-
-## How it Works
-```
-my_list = [1, 2, 3, 1, 4, 2, 5]
-
-set(my_list) → {1, 2, 3, 4, 5}  # removes duplicates
-sum({1, 2, 3, 4, 5}) → 15 
+    return sum(set(my_list)) 
