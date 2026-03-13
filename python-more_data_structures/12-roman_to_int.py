f0bffce6750fae713d346b1954a8e9e7e639d43a
didier-abizera <d.abizera@alustudent.com>
2026-03-12 22:54:44 -0700

    Add roman_to_int function

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for roman_to_int function"""
+
+
+def roman_to_int(roman_string):
+    """Converts a Roman numeral to an integer"""
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    romans = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    prev = 0
+
+    for char in reversed(roman_string):
+        curr = romans[char]
+        if curr < prev:
+            result -= curr
+        else:
+            result += curr
+        prev = curr
+
+    return result
