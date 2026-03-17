7130204a07ab31cc10aaffd3d38a91dabcb21416
didier-abizera <d.abizera@alustudent.com>
2026-03-16 22:04:44 -0700

    Add Square class with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+
+class Square:
+    """Class that defines a square by size"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
