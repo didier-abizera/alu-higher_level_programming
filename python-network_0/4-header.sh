15e0532fb8b77a219588ffcb8076c39b1ff48f31
didier-abizera <d.abizera@alustudent.com>
2026-03-27 08:32:59 -0700

    Move 4-header.sh to correct directory

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with header X-HolbertonSchool-User-Id: 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with a custom header to a URL
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
