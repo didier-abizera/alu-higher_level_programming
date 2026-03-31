c0eeed97644d3438b72aab06e0be15a4f1c5efab
didier-abizera <d.abizera@alustudent.com>
2026-03-31 16:10:59 -0700

    dfgh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Sends a POST request with specific parameters and displays the response body
-
-curl -s -X POST "$1" \
--d "email=test@gmail.com" \
--d "subject=I will always be here for PLD"
+# Curl sends POST req to URL, display response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
